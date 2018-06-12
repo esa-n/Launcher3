ev-staging/android_packages_apps_Launcher3

Launcher3: Only fail for the current user
Change-Id: Ia97edb50fea18586776958e5be967b0f849eb2b8

src/com/android/launcher3/IconCache.java
@@ -251,9 +251,12 @@ public class IconCache {
             // Query for the set of apps
             final List<LauncherActivityInfoCompat> apps = mLauncherApps.getActivityList(null, user);
             // Fail if we don't have any apps
-            // TODO: Fix this. Only fail for the current user.
             if (apps == null || apps.isEmpty()) {
-                return;
+                if (user.equals(UserHandleCompat.myUserHandle())) {
+                    return;
+                } else {
+                    continue;
+                }
             }
 
             // Update icon cache. This happens in segments and {@link #onPackageIconsUpdated}

esa-n/Launcher3

src/com/android/launcher3/IconCache.java
@@ -244,9 +244,12 @@ public class IconCache {
             // Query for the set of apps
             final List<LauncherActivityInfo> apps = mLauncherApps.getActivityList(null, user);
             // Fail if we don't have any apps
-            // TODO: Fix this. Only fail for the current user.
             if (apps == null || apps.isEmpty()) {
-                return;
+                if (user.equals(Process.myUserHandle())) {
+                    return;
+                } else {
+                    continue;
+                }
             }
 
             // Update icon cache. This happens in segments and {@link #onPackageIconsUpdated}